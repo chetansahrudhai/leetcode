fdb15b41f08ff21d2aeb41df91d0ee0217b91ad9
Chetan Sahrudhai Kimidi <56767652+chetansahrudhai@users.noreply.github.com>
2025-11-14 23:46:20 +0530

    202 - Happy Number

---
+++ 202_happyNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        hashset = set()
+        while n != 1:
+            if n in hashset: return False
+            hashset.add(n)
+            n = sum([int(i) ** 2 for i in str(n)])
+        else:
+            return True
