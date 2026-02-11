1782efac8d7d47316843d8bebf08bea0d6417cca
Chetan Sahrudhai Kimidi <56767652+chetansahrudhai@users.noreply.github.com>
2026-02-11 23:30:45 +0530

    3721 - Longest Balanced Subarray II
    version 1 - thanks ItzKanashii on LC, i gotta improve.

---
+++ 3721_longestBalancedSubarray2_v1.py
@@ -0,0 +1,60 @@
+class SegmenTree:
+    def __init__(self, n):
+        self.n = n
+        self.mn = [0]*(4*n)
+        self.mx = [0]*(4*n)
+        self.lazy = [0]*(4*n)
+
+    def push(self, i):
+        if self.lazy[i]:
+            for c in (2*i, 2*i+1):
+                self.mn[c] += self.lazy[i]
+                self.mx[c] += self.lazy[i]
+                self.lazy[c] += self.lazy[i]
+            self.lazy[i] = 0
+
+    def add(self, i, L, R, l, r, v):
+        if r < L or R < l:
+            return
+        if l <= L and R <= r:
+            self.mn[i] += v
+            self.mx[i] += v
+            self.lazy[i] += v
+            return
+        self.push(i)
+        M = (L+R)//2
+        self.add(2*i, L, M, l, r, v)
+        self.add(2*i+1, M+1, R, l, r, v)
+        self.mn[i] = min(self.mn[2*i], self.mn[2*i+1])
+        self.mx[i] = max(self.mx[2*i], self.mx[2*i+1])
+
+    def zero(self, i, L, R, ql, qr):
+        if qr < L or R < ql:
+            return -1
+        if self.mn[i] > 0 or self.mx[i] < 0:
+            return -1
+        if L == R:
+            return L
+        self.push(i)
+        M = (L+R)//2
+        x = self.zero(2*i, L, M, ql, qr)
+        if x != -1:
+            return x
+        return self.zero(2*i+1, M+1, R, ql, qr)
+
+class Solution:
+    def longestBalanced(self, nums: List[int]) -> int:
+        res = 0
+        n = len(nums)
+        ST = SegmenTree(n)
+        last = {}
+        
+        for r, x in enumerate(nums):
+            p = last.get(x, -1)
+            w = 1 if x % 2 == 0 else -1
+            ST.add(1, 0, n-1, p+1, r, w)
+            l = ST.zero(1, 0, n-1, 0, r)
+            if l != -1: res = max(res, r-l+1)
+            last[x] = r
+        
+        return res
